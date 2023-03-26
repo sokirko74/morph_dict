697b0f9bb0d72409ff967fda574774ab76859162
Andrei <soulcatcher13@gmail.com>
2023-03-26 21:42:53 +0300

    Fix build with clang 13 on FreeBSD

--- LemmatizerBaseLib/MorphAutomat.h
+++ LemmatizerBaseLib/MorphAutomat.h
@@ -118,7 +118,7 @@ public:
 struct CFuzzyResult {
 	std::string CorrectedString;
 	int StringDistance;
-	bool operator < (const  CFuzzyResult& x) {
+	bool operator < (const  CFuzzyResult& x) const {
 		return StringDistance < x.StringDistance;
 	}
 };
