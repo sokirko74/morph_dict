c3e7f78d8eeb05f2535c9115021613c1deb5c34d
sokirko74 <alexey.sokirko@gmail.com>
2023-11-25 22:48:17 +0300

    fix namespace

--- homon_freq_bin/homon_freq_bin.cpp
+++ homon_freq_bin/homon_freq_bin.cpp
@@ -209,7 +209,7 @@ int main(int argc, const char** argv) {
         if (saveBin(args.CloseOutputStreamAndGetName()))
             return 0;
     }
-    catch (std::exception e) {
+    catch (std::exception& e) {
         LOGE << "exception occurred:" << e.what();
     }
     catch (...) {
